81f0e94bdfdbbb960cb9dcc689a95f4da97a6df7
michaeljfriedman <mfred240@gmail.com>
2016-09-05 22:50:58 -0400

    Initial commit

---
+++ Coordinate.java
@@ -0,0 +1,17 @@
+/***********************************************************************************
+* Author: Michael Friedman
+* Created: 9/5/16
+*
+*
+* Description: This is an interface for a Coordinate. It is a wrapper for a value,
+* intended to provide strict typing for x, y, and z coordinates separately
+***********************************************************************************/
+
+public interface Coordinate {
+
+	// Instance variables
+	private int value;
+
+	// Methods
+	public int value();
+}
